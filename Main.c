4aea32deb62f3c339ffe36e7bd9819ef5c9b5b18
HendrikStoeber <hendrik_stoeber@web.de>
2025-12-19 20:04:04 +0800

    Implemented Mult in Main to test

--- Main.c
+++ Main.c
@@ -3,6 +3,27 @@
 
 int main()
 {
-    
+    //No specific requirements, therefore simplified, used only Multiplication
+    int A[SIZE][SIZE]={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int B[SIZE][SIZE]={
+        {9,8,7},
+        {6,5,4},
+        {3,2,1}
+    };
+    int res[SIZE][SIZE];
+    Mult(A,B,res);
+
+    for(int i=0;i<SIZE;i++)
+    {
+        for(int j=0;j<SIZE;j++)
+        {
+            printf("%d ",res[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
