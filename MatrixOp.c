36df10a0caacdc2af8d02ed301e2795cc9ac65f1
HendrikStoeber <hendrik_stoeber@web.de>
2025-12-19 10:58:52 +0800

    Fixed Header Param. Created MatrixOp.c

---
+++ MatrixOp.c
@@ -0,0 +1,12 @@
+#include "MatrixOp.h"
+int* Add(int A[SIZE][SIZE], int B[SIZE][SIZE])
+{
+    int sum[SIZE][SIZE];
+    for(int i=0;i<SIZE;i++)
+    {
+        for(int j=0;j<SIZE;j++)
+        {
+            sum[i][j]=A[i][j];
+        }
+    }
+}
--- MatrixOp.h
+++ MatrixOp.h
@@ -2,15 +2,15 @@
 #define MATRIX_OP_H
 #define SIZE 3
 //Basic
-int* Add(int *A, int *B, int size);
-int* Sub(int *A, int *B, int size);
-int* Dot(int *A, int *B, int size);
+int* Add(int A[SIZE][SIZE], int B[SIZE][SIZE]);
+int* Sub(int A[SIZE][SIZE], int B[SIZE][SIZE]);
+int* Dot(int A[SIZE][SIZE], int B[SIZE][SIZE]);
 //Linear
-int* Mult(int *A, int *B, int size);
-int* Trp(int *A, int *B, int size);
+int* Mult(int A[SIZE][SIZE], int B[SIZE][SIZE]);
+int* Trp(int A[SIZE][SIZE]);
 //Advanced
-int* Det(int *A, int *B, int size);
-int* Adj(int *A, int *B, int size);
+int Det(int A[SIZE][SIZE]);
+int* Adj(int A[SIZE][SIZE]);
 //Inverse
-int* Inv(int *A, int *B, int size);
+int* Inv(int A[SIZE][SIZE]);
 #endif
